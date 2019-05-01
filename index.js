6c0b18ba7e0862c008ce5bd9c6c3a10fbf0a9128
SiriusTsou <sirius.tsou@gmail.com>
2019-05-01 17:59:21 +0800

    add eslint

--- index.js
+++ index.js
@@ -1,147 +1,139 @@
-const express = require('express');
-const path = require('path');
-const util = require('util');
+const express = require('express')
+const path = require('path')
+const util = require('util')
 const session = require('express-session')
 const bodyParser = require('body-parser')
 const morgan = require('morgan')
 const jwt = require('jsonwebtoken')
-const Eauth = require('express-eauth');
-const async = require("async");
+const Eauth = require('express-eauth')
+const async = require('async')
 const MobileDetect = require('mobile-detect')
+const cookieParser = require('socket.io-cookie-parser')
+// initalize sequelize with session store
+const SequelizeStore = require('connect-session-sequelize')(session.Store)
 
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/config/config.json')[env];
+const env = process.env.NODE_ENV || 'development'
 
-const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const app = express()
+const server = require('http').Server(app)
+const io = require('socket.io')(server)
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 
 // LOG
-if (app.get('env') === 'development')
-  app.use(morgan('dev'))
+if (app.get('env') === 'development') app.use(morgan('dev'))
+
+const config = require('./config/config.json')[env]
 
 // issue, dev // maybe add salt with secret
 app.set('secret', config.secret)
 
 // initalize database
-const db = require('./models');
-const User = db.User;
-const Session = db.Session;
+const db = require('./models')
+
+const { User, Session } = db
 
 // creat database if not exist // if force == true : drop table
-async function initalize(){
-  await User.sync();
-  await Session.sync({force: true});
-};
-initalize();
+async function initalize() {
+  await User.sync()
+  await Session.sync({ force: true })
+}
+initalize()
 
-// initalize sequelize with session store
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const sequelizeStore = new SequelizeStore({
   db: db.sequelize,
-  table: 'Session'
+  table: 'Session',
 })
 
 app.use(session({
   secret: app.get('secret'),
   store: sequelizeStore,
   resave: false,
-  saveUninitialized: true
+  saveUninitialized: true,
 }))
 
 // Add body parser.
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
 // eauth
-const eauth1 = new Eauth({banner: config.banner});
-const eauth2 = new Eauth({banner: config.banner, method: 'personal_sign'});
+const eauth1 = new Eauth({ banner: config.banner })
+const eauth2 = new Eauth({ banner: config.banner, method: 'personal_sign' })
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', async (req, res) => {
-  if(req.session.address)
-    res.render('logout', {address:req.session.address});
+  if (req.session.address) res.render('logout', { address: req.session.address })
   else {
-    md = new MobileDetect(req.headers['user-agent']);
-    if (md.mobile()) {
-      res.render('index', {method:'personal_sign'})
-    }
-    else {
-      res.render('index', {method:'eth_signTypedData', qrcode:true})
-    }
+    const md = new MobileDetect(req.headers['user-agent'])
+    if (md.mobile()) res.render('index', { method: 'personal_sign' })
+    else res.render('index', { method: 'eth_signTypedData', qrcode: true })
   }
-});
+})
 
-const cookieParser = require('socket.io-cookie-parser')
-io.use(cookieParser());
-io.on('connection', function (socket) {
-  socket.emit('init', { session_id: socket.request.cookies['connect.sid'].substr(2,32) });
-  setTimeout(()=>{ socket.emit('refresh');socket.disconnect(true);console.log('socket disconnect') }, 180000);
-});
+
+io.use(cookieParser())
+io.on('connection', (socket) => {
+  socket.emit('init', { session_id: socket.request.cookies['connect.sid'].substr(2, 32) })
+  setTimeout(() => { socket.emit('refresh'); socket.disconnect(true); console.log('socket disconnect') }, 180000)
+})
 
 async function eauthMiddleware(req, res, next) {
-  var middleware = eauth1;
-  md = new MobileDetect(req.headers['user-agent']);
-  if (md.mobile())
-    middleware = eauth2;
+  let middleware = eauth1
+  const md = new MobileDetect(req.headers['user-agent'])
+  if (md.mobile()) middleware = eauth2
 
-  async.series([middleware.bind(null, req, res)], function(err) {
-    if(err)
-      return next(err);
-    next();
-  });
+  async.series([middleware.bind(null, req, res)], (err) => {
+    return err ? next(err) : next()
+  })
 }
 
 // return Address or Confirm Code or status 400
 app.get('/auth/:Address', eauthMiddleware, (req, res) => {
-  req.eauth.message ? res.send(req.eauth.message) : res.status(400).send();
-});
+  return req.eauth.message ? res.send(req.eauth.message) : res.status(400).send()
+})
 
 // return Address or status 400
 app.post('/auth/:Message/:Signature', eauthMiddleware, (req, res) => {
-  const address = req.eauth.recoveredAddress;
+  const address = req.eauth.recoveredAddress
 
-  if (!address)
-    res.status(400).send();
+  if (!address) res.status(400).send()
   else {
-    User.findOrCreate({ where: {"address": address} }).spread( (eauth,created) => {
+    User.findOrCreate({ where: { address: address } }).spread((eauth, created) => {
       const token = jwt.sign(eauth.get({ plain: true }), app.get('secret'), {
-        expiresIn: 60*15*1000 // session expire time deafault hardcode 15 min // SHOULD CONFIG
+        expiresIn: 60 * 15 * 1000, // session expire time deafault hardcode 15 min // SHOULD CONFIG
       })
 
-      req.session.cookie.expires = 60*15*1000; // session expire time deafault hardcode 15 min // SHOULD CONFIG
-      req.session.address_id = eauth.dataValues.id; // database id // oauth use
-      req.session.address = address;
-      req.session.token = token;
+      req.session.cookie.expires = 60 * 15 * 1000 // session expire time deafault hardcode 15 min // SHOULD CONFIG
+      req.session.address_id = eauth.dataValues.id // database id // oauth use
+      req.session.address = address
+      req.session.token = token
 
       res.json({
         success: true,
         message: 'Eauth Success',
-        token: token
+        token: token,
       })
-    });
+    })
   }
-});
+})
 
 function apiMiddleware(req, res, next) {
-  const token = req.session.token;
+  const { token } = req.session
+
   if (token) {
     // issue case: after server restart will pass verify cond,but token is expire, maybe should check database
-    jwt.verify(token, app.get('secret'), function (err, decoded) {
+    jwt.verify(token, app.get('secret'), (err, decoded) => {
       if (err) {
-        return res.json({success: false, message: 'Failed to authenticate token.'})
-      } else {
-        req.decoded = decoded
-        next()
-      }
+        return res.json({ success: false, message: 'Failed to authenticate token.' })
+      } 
+        
+      req.decoded = decoded
+      return next()
     })
-  } 
-  else {
-    url = req.url ? util.format('/?url=%s', encodeURIComponent(req.url)) : '/';
+  } else {
+    const url = req.url ? util.format('/?url=%s', encodeURIComponent(req.url)) : '/'
     res.redirect(url)
   }
 }
@@ -149,53 +141,51 @@ function apiMiddleware(req, res, next) {
 // oauth server # todo: make it optional
 require('./components/oauth')(app, apiMiddleware)
 
-const api = express.Router();
+const api = express.Router()
 
 // api middleware
-api.use(apiMiddleware);
+api.use(apiMiddleware)
 
 // api logout
 app.all('/logout', api, (req, res) => {
-  req.session.destroy((err)=>{
-    location = '/'
-    if(req.body.url)
-      location = req.body.url
+  req.session.destroy((err) => {
+    let location = '/'
+    if (req.body.url) location = req.body.url
     res.redirect(location)
-  });
-});
+  })
+})
 
 app.get('/user', api, (req, res) => {
   res.json({
     success: true,
-    message: req.session.address
-  });
-});
+    message: req.session.address,
+  })
+})
 
 app.get('/qrcode', api, async (req, res) => {
   // set session to logined
-  if ( req.query.session_id && req.query.socket_id ) {
-    s =  await sequelizeStore.get(req.query.session_id)
-    if ( s )
+  if (req.query.session_id && req.query.socket_id) {
+    if (await sequelizeStore.get(req.query.session_id)) {      
       await sequelizeStore.set(req.query.session_id, req.session)
+    }
     // emit loggin message
-    socket = io.clients().sockets[req.query.socket_id]
-    if ( socket)
-      await socket.emit('refresh')
+    const socket = io.clients().sockets[req.query.socket_id]
+    if (socket) await socket.emit('refresh')
   }
 
   // clean client session
   res.redirect('/logout')
-});
+})
 
 // error handler
-app.use(function(err, req, res, next) {
-  res.status(err.status || 500);
+app.use((err, req, res, next) => {
+  res.status(err.status || 500)
   res.render('error', {
     message: err.message,
-    error: app.get('env') === 'development' ? err : {}
-  });
-});
+    error: app.get('env') === 'development' ? err : {},
+  })
+})
 
-var listener = server.listen(process.env.PORT || 8080, () => {
+const listener = server.listen(process.env.PORT || 8080, () => {
   console.log('Listening on port ' + listener.address().port)
 })
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,99 +1,93 @@
-let domain = "";
-let confirmcode = null;
-let signature = null;
-let access_token = null;
-let fm = new Fortmatic('pk_live_CC75CEEE7D7E8630');
-let fortmatic_web3 = null
+const domain = ''
+let accessToken = null
+const fm = new Fortmatic('pk_live_CC75CEEE7D7E8630')
+let fortmaticWeb3 = null
 
 function authStart(_web3) {
   return $.get(domain + '/auth/' + _web3.eth.accounts[0], (res) => {
-    data = ''
-    meassage = ''
-    if ( $("#method")[0].value === 'personal_sign' ) {
-      data = "0x"+Array.from(res).map(x=>x.charCodeAt(0).toString(16)).join('');
+    let data = ''
+    let message = ''
+
+    if ($('#method')[0].value === 'personal_sign') {
+      data = '0x' + Array.from(res).map(x => x.charCodeAt(0).toString(16)).join('')
       message = res
-    }
-    else if ( $("#method")[0].value === 'eth_signTypedData' ) {
+    } else if ($('#method')[0].value === 'eth_signTypedData') {
       data = res
       message = res[1].value
     }
 
     // Call metamask to sign
-    const from = _web3.eth.accounts[0];
-    const params = [data, from];
-    const method = $("#method")[0].value;
+    const from = _web3.eth.accounts[0]
+    const params = [data, from]
+    const method = $('#method')[0].value
     _web3.currentProvider.sendAsync({
-      id:1,
+      id: 1,
       method,
-      params
+      params,
     }, async (err, result) => {
-      if (err) {
-        return console.error(err);
-      }
-      if (result.error) {
-        return console.error(result.error);
-      }
-      signature = result.result;
+      if (err) return console.error(err)
+      if (result.error) return console.error(result.error)
+      
+      const signature = result.result
 
-      if (message !== null && signature !== null){
+      if (message !== null && signature !== null) {
         $.post(domain + '/auth/' + message + '/' + signature, (res) => {
           if (res.success) {
-            access_token = res.token;
-            console.log("Eauth Success")
-            console.log("Your JWT token: " + access_token)
-            var url_string = document.URL; //window.location.href
-            var url = new URL(url_string);
+            accessToken = res.token
+            console.log('Eauth Success')
+            console.log('Your JWT token: ' + accessToken)
+            const url = new URL(document.URL)
 
-            var location = '/'
-            var c = url.searchParams.get("url");
-            var q = url.searchParams.get("socket_id");
-            var s = url.searchParams.get("session_id");
-            if (c)
-              location = c;
-            else if (q && s)
-              location = '/qrcode?socket_id='+q+'&session_id='+s
+            let location = '/'
+            const c = url.searchParams.get('url')
+            const q = url.searchParams.get('socket_id')
+            const s = url.searchParams.get('session_id')
+            if (c) {
+              location = c
+            } else if (q && s) {
+              location = '/qrcode?socket_id=' + q + '&session_id=' + s
+            }
             window.location = location
           } else {
-            console.log("Eauth Failed")
+            console.log('Eauth Failed')
           }
-        });
+        })
       }
-    });
-  }).fail(function() {
-    document.getElementById("test").innerText='test1'
-    // alert('woops'); // or whatever
-  });
+      return true
+    })
+  })
 }
 
-$(()=>{
-  fortmatic_web3 = new Web3(fm.getProvider());
-});
+$(() => {
+  fortmaticWeb3 = new Web3(fm.getProvider())
+  $('.eth-signin')[0].disabled = false
+  $('.fortmatic-signin')[0].disabled = false
+})
 
-$('.eth-signin').on('click', function () {
+$('.eth-signin').on('click', () => {
   // Detect metamask
   if (typeof web3 !== 'undefined') {
-    console.log("web3 is detected.");
+    console.log('web3 is detected.')
   } else {
-    return alert("No web3 detected. Please install metamask");
+    return alert('No web3 detected. Please install metamask')
   }
-  
-  if ( web3.currentProvider.isMetaMask === true && web3.eth.accounts[0] === undefined)
-    return alert("Please login metamask first.");
 
-  if (web3.currentProvider.enable)
+  if (web3.currentProvider.enable) {
     web3.currentProvider.enable()
-    .then(function() {
-      authStart(web3);
-    });
-  else if (web3.eth.accounts[0]) {
-    authStart(web3);
+    .then(() => {
+      if (web3.currentProvider.isMetaMask === true && web3.eth.accounts[0] === undefined) {
+        return alert('Please login metamask first.')
+      }
+      authStart(web3)
+    })
+  } else if (web3.eth.accounts[0]) {
+    authStart(web3)
   }
-});
+})
 
-$('.fortmatic-signin').on('click', function () {
-  fortmatic_web3.currentProvider.enable()
-  .then(function() {
-    authStart(fortmatic_web3);
-  });
-});
-
+$('.fortmatic-signin').on('click', () => {
+  fortmaticWeb3.currentProvider.enable()
+  .then(() => {
+    authStart(fortmaticWeb3)
+  })
+})

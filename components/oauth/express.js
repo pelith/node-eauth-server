e7077417cbc2401f6ce5a4f841fbaef74015cb46
sirius <sirius.tsou@gmail.com>
2019-08-27 18:04:36 +0800

    update ens method

--- components/oauth/express.js
+++ components/oauth/express.js
@@ -11,10 +11,31 @@ const OAuthClient = db.OAuthClient;
 // initalize oauth2 server
 const oauth = require('./oauth')
 
+// ENS HOOK
+const env = process.env.NODE_ENV || 'development'
+const config = require('./config/config.json')[env]
+const ENS = require('ethereum-ens')
+const Web3 = require('web3')
+const provider = new Web3.providers.HttpProvider(config.rpcURL)
+const ens = new ENS(provider)
+
 module.exports = function(app, middleware, User){
   // only private can get
   app.get('/oauth/user', authenticate(), function(req, res) {
-    return res.json(req.user.User)
+    // ENS HOOK
+    let ens_name = null
+    try {
+      const reverse_name = await ens.reverse(req.user.User.address).name()
+      const name = await ens.resolver(reverse_name).addr()
+
+     // Check to be sure the reverse record is correct.
+      if (req.user.User.address.toLowerCase() == name.toLowerCase())
+        ens_name = reverse_name
+    } catch (e) {
+      console.log(e)
+    }
+
+    return ens_name ? res.json(Object.assign(req.user.User, {ens: ens_name})) : res.json(req.user.User)
   });
 
   app.all('/oauth/token', function(req,res,next){

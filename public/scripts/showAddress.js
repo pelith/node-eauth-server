6d8c2b267160c3ca6370019da25db196caa4e9a3
N.M <freedomtomdestiny@gmail.com>
2019-09-01 15:49:54 +0800

    Use tooltip & update styles

--- public/scripts/showAddress.js
+++ public/scripts/showAddress.js
@@ -4,16 +4,9 @@ const address = document.getElementById('address')
 const prefixAddress = fulladdress.slice(0, 8)
 const suffixAddress = fulladdress.slice(fulladdress.length - 8, fulladdress.length)
 const partialAddress = `${prefixAddress}...${suffixAddress}`
+address.textContent = partialAddress
 
-address.addEventListener('click', showFullAddress)
-address.style.display = null;
 
-function showFullAddress() {
-  if (fulladdress.length > 18 && address.textContent === fulladdress)
-    address.textContent = partialAddress
-  else
-    address.textContent = fulladdress
-}
-
-if (fulladdress.length > 18) address.textContent = partialAddress
-else showFullAddress()
+$(document).ready(function(){
+	$('[data-toggle="tooltip"]').tooltip()
+})

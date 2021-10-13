95c9a77a28e874ff47207d1bc21e2732002d460f
hung-pelith <hung@pelith.com>
2021-10-13 17:27:41 +0800

    fix: ensure the user table is synced before inserting client

--- components/oauth/index.js
+++ components/oauth/index.js
@@ -1,8 +1,10 @@
-const db = require('./models')
+const async = require('async')
+const OAuthDB = require('./models')
 
-const { OAuthClient } = db
+const { User, OAuthClient } = OAuthDB
 
 async function initalize() {
+  await User.sync()
   await OAuthClient.sync()
   if (process.env.EAUTH_CLIENT_NAME && process.env.EAUTH_CLIENT_ID && process.env.EAUTH_CLIENT_SECRET && process.env.EAUTH_REDIRECT_URI) {
     await OAuthClient.findOrCreate({
@@ -11,6 +13,10 @@ async function initalize() {
         client_id: process.env.EAUTH_CLIENT_ID,
         client_secret: process.env.EAUTH_CLIENT_SECRET,
         redirect_uri: process.env.EAUTH_REDIRECT_URI,
+      },
+      defaults: {
+        grant_types: null,
+        scope: null,
       },
     })
   }
